ba09f52e2a8e232597a925a3d9d755f15a91151d
Joshua Kaminetsky <jmkaminetsky@gmail.com>
2024-09-13 16:56:17 -0700

    display profile when logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { FormEvent, useState } from "react";
 
+import { useAuth } from "./auth/use-auth-context";
 import Login from "./auth/Login";
 
 import "./App.css";
+import Profile from "./profile/Profile";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { isLoggedIn } = useAuth();
 
-  return <Login />;
+  return (
+    <>
+      {isLoggedIn && <Profile />}
+      {!isLoggedIn && <Login />}
+    </>
+  );
 }
 
 export default App;
