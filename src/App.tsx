347f53222044e99a2c7fd592817db98185893b07
Joshua Kaminetsky <jmkaminetsky@gmail.com>
2024-09-13 16:18:13 -0700

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { FormEvent, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { FormEvent } from "react";
 import "./App.css";
 
 function App() {
