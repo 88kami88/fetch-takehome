2b0fd18b9b028cec43c5bcd1e031821cc0ddf023
Joshua Kaminetsky <jmkaminetsky@gmail.com>
2024-09-13 18:45:58 -0700

    pagination working

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
-const baseUrl = "https://frontend-take-home-service.fetch.com";
+export const baseUrl = "https://frontend-take-home-service.fetch.com";
 
 export const authUrl = `${baseUrl}/auth/login`;
-export const breedsUrl = `${baseUrl}/dogs/breeds`;
 export const dogsUrl = `${baseUrl}/dogs`;
-export const searchUrl = `${baseUrl}/dogs/search`;
+export const searchPath = `/search`;
--- src/dogs/Dogs.tsx
+++ src/dogs/Dogs.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { dogsUrl, searchUrl } from "../constants";
+import { baseUrl, dogsUrl, searchPath } from "../constants";
 import { DogCard } from "./DogCard";
 
 type SearchResultIds = string[];
@@ -20,44 +20,78 @@ export interface Dog {
   breed: string;
 }
 
+async function searchDogs(path: string): Promise<[SearchResponse, Dog[]]> {
+  console.log({ path });
+  const searchRes = await fetch(`${baseUrl}${path}`, {
+    credentials: "include",
+  });
+
+  // TODO handle error
+
+  const searchJson = (await searchRes.json()) as SearchResponse;
+  const resultIds: SearchResultIds = searchJson.resultIds;
+
+  const dogsRes = await fetch(dogsUrl, {
+    body: JSON.stringify(resultIds),
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  });
+
+  // TODO handle error
+
+  const dogsJson = (await dogsRes.json()) as Dog[];
+
+  return [searchJson, dogsJson];
+}
+
 export default function Dogs() {
   const [dogs, setDogs] = useState<Dog[]>([]);
+  const [next, setNext] = useState<string | undefined>();
+  const [prev, setPrev] = useState<string | undefined>();
+
+  async function onSearch(path = `/dogs${searchPath}`) {
+    const [search, dogs] = await searchDogs(path);
+
+    setNext(search.next);
+    setPrev(search.prev);
+
+    setDogs(dogs);
+  }
+
+  function onPrev() {
+    if (!prev) {
+      return;
+    }
+
+    onSearch(prev);
+  }
+
+  function onNext() {
+    if (!next) {
+      return;
+    }
+
+    onSearch(next);
+  }
 
   useEffect(() => {
-    (async () => {
-      const searchRes = await fetch(searchUrl, {
-        credentials: "include",
-      });
-
-      // TODO handle error
-
-      const searchJson = (await searchRes.json()) as SearchResponse;
-      const resultIds: SearchResultIds = searchJson.resultIds;
-
-      const dogsRes = await fetch(dogsUrl, {
-        body: JSON.stringify(resultIds),
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-      });
-
-      // TODO handle error
-
-      const dogsJson = (await dogsRes.json()) as Dog[];
-
-      setDogs(dogsJson);
-    })();
+    onSearch();
   }, []);
 
   return (
-    <ul>
-      {dogs.map((dog) => (
-        <li key={dog.id}>
-          <DogCard dog={dog} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {dogs.map((dog) => (
+          <li key={dog.id}>
+            <DogCard dog={dog} />
+          </li>
+        ))}
+      </ul>
+      {prev && <button onClick={onPrev}>Prev</button>}
+      {next && <button onClick={onNext}>Next</button>}
+    </>
   );
 }
