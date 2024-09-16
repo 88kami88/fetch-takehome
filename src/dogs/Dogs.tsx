b2063a0a635c1331f6d3a73f52eeedd953205b05
Joshua Kaminetsky <jmkaminetsky@gmail.com>
2024-09-16 16:06:08 -0700

    fixed prev/next

--- src/dogs/Dogs.tsx
+++ src/dogs/Dogs.tsx
@@ -12,16 +12,24 @@ export default function Dogs() {
   const [dogs, setDogs] = useState<Dog[]>([]);
   const [next, setNext] = useState<string | undefined>();
   const [prev, setPrev] = useState<string | undefined>();
+  const [error, setError] = useState<string | undefined>();
   const { breeds, sort } = useBreed();
 
   const onSearch = useCallback(
     async (path: string, isCursor: boolean) => {
-      const [search, dogs] = await searchDogs(path, breeds, sort, isCursor);
+      setDogs([]);
+      setError(undefined);
 
-      setNext(search.next);
-      setPrev(search.prev);
+      try {
+        const [search, dogs] = await searchDogs(path, breeds, sort, isCursor);
 
-      setDogs(dogs);
+        setNext(search.next);
+        setPrev(search.prev);
+
+        setDogs(dogs);
+      } catch {
+        setError("Unable to fetch dogs");
+      }
     },
     [breeds, setDogs, setNext, setPrev, sort]
   );
@@ -46,16 +54,36 @@ export default function Dogs() {
     onSearch(`/dogs${searchPath}`, false);
   }, [breeds, onSearch, sort]); // must re-search when breed or sort changes even though it's not a direct dependency
 
+  if (error) {
+    return error;
+  }
+
   return (
     <>
-      <ul className="dogs">
-        {dogs.map((dog) => (
-          <li key={dog.id}>
-            <DogCard dog={dog} />
-          </li>
-        ))}
-      </ul>
-      <Box sx={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
+      <Box sx={{ paddingBottom: "100px" }}>
+        <ul className="dogs">
+          {dogs.map((dog) => (
+            <li key={dog.id}>
+              <DogCard dog={dog} />
+            </li>
+          ))}
+        </ul>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "1rem",
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          width: "100%",
+          justifyContent: "center",
+          padding: "10px 20px",
+          backgroundColor: "white",
+          boxShadow: "0 -2px 5px rgba(0,0,0,0.1)",
+        }}
+      >
         {prev && <button onClick={onPrev}>Prev</button>}
         {next && <button onClick={onNext}>Next</button>}
       </Box>
